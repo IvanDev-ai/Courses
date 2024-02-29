59e41145b3505ccb9a3d9869584f3b4bafe4af7c
IvanDev <i.martin.desarrollo@gmail.com>
2024-02-29 16:03:10 +0100

    update

--- cnn1/main.py
+++ cnn1/main.py
@@ -1,15 +1,25 @@
 import streamlit as st
-import pandas as pd 
 from keras.models import load_model
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
-from io import BytesIO
+import pandas as pd
+import gdown
 
-model = None
-uploaded_file = st.file_uploader("Cargar modelo entrenado", type=["h5"])
-if uploaded_file is not None:
-    model_file = BytesIO(uploaded_file.read())
-    model = load_model(model_file)
+MAX_SEQUENCE_LENGTH = 100
+
+# Descargar el archivo CSV desde Google Drive
+url = 'https://drive.google.com/uc?id=1Sf7AjBoEC1-ZtkrmAnq0frTWMAuh5osb'
+output = 'train.csv'
+gdown.download(url, output, quiet=False)
+
+train = pd.read_csv(output)
+sentences = train["comment_text"].fillna("DUMMY_VALUE").values
+
+# Cargar el modelo
+def load_h5_model():
+    return load_model("modelo_entrenado.h5")
+
+model = load_h5_model()
 
 MAX_SEQUENCE_LENGTH = 100
 
@@ -18,6 +28,7 @@ st.title('Clasificador de Comentarios')
 st.write("Solo responde en ingles ya que fue entrenado con una base de datos anglosajona!")
 
 tokenizer = Tokenizer()
+tokenizer.fit_on_texts(sentences)
 
 # Procesamiento de la entrada del usuario
 input_text = st.text_input('Ingrese un comentario:', 'you are an idiot')
@@ -25,13 +36,12 @@ input_sequence = tokenizer.texts_to_sequences([input_text])
 input_sequence = pad_sequences(input_sequence, maxlen=MAX_SEQUENCE_LENGTH)
 
 # Predicción
-if model is not None:
-    predictions = model.predict(input_sequence)
+predictions = model.predict(input_sequence)
 
-    possible_labels = ["toxic","severe_toxic","obscene","threat","insult","identity_hate"]
+possible_labels = ["toxic","severe_toxic","obscene","threat","insult","identity_hate"]
 
-    # Mostrar el resultado de la predicción
-    st.write('Predicciones:')
-    for label, prob in zip(possible_labels, predictions[0]):
-        st.write(f"{label}:")
-        st.progress(prob.astype(float))  # Convertir a tipo float
+# Mostrar el resultado de la predicción
+st.write('Predicciones:')
+for label, prob in zip(possible_labels, predictions[0]):
+    st.write(f"{label}:")
+    st.progress(prob.astype(float))  # Convertir a tipo float
